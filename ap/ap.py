81ce60b6dd58abbde4116dcc0d35a66fcb1e7b70
Brian Cui <brian.cui@live.com>
2018-04-02 17:57:25 -0500

    Created separate useful utils file

--- ap/ap.py
+++ ap/ap.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+from utils import print_stdout
 MIN_ARGS = 1
 ARGUMENTS = sys.argv[1:]
 
@@ -16,6 +17,6 @@ except:
     print("Error: network interface \"{0}\" does not exist.".format(ARGUMENTS[0]))
     sys.exit(1)
 
-print(ifconfig.stdout)
+print_stdout(ifconfig)
 print("You did it!")
 
---
+++ ap/utils.py
@@ -0,0 +1,2 @@
+def print_stdout(completed_process):
+    print(completed_process.stdout.decode("utf-8"), end="")
