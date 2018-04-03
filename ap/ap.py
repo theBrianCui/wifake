936319b20fc9c2a121f3d4194da64642fbda856d
Brian Cui <brian.cui@live.com>
2018-04-02 20:11:38 -0500

    ap now runs airmon-ng

--- ap/ap.py
+++ ap/ap.py
@@ -1,22 +1,36 @@
 import sys
 import subprocess
 from utils import print_stdout
+
 MIN_ARGS = 1
 ARGUMENTS = sys.argv[1:]
 
 # check arguments
 if len(ARGUMENTS) < MIN_ARGS:
-    print("Usage: python3 ap.py [interface]")
+    print("Usage: python3 ap.py interface")
     sys.exit(1)
 
-# ensure the network interface exists
+INTERFACE = ARGUMENTS[0]
+
+# ensure the network interface exists, and is wireless
+print("Checking interface {0}... ".format(INTERFACE), end="", flush=True)
 iwconfig = None
 try:
-    iwconfig = subprocess.run(["iwconfig", ARGUMENTS[0]], stdout=subprocess.PIPE, check=True)
+    iwconfig = subprocess.run(["iwconfig", INTERFACE], stdout=subprocess.PIPE, check=True)
 except:
-    print("Error: network interface \"{0}\" does not exist or is not wireless.".format(ARGUMENTS[0]))
+    print("Error: network interface \"{0}\" does not exist or is not wireless.".format(INTERFACE))
     sys.exit(1)
+print("Done.")
 
-print_stdout(iwconfig)
+# airmon-ng check kill
+print("Executing `airmon-ng check kill`... ", end="", flush=True)
+try:
+    subprocess.run(["airmon-ng", "check", "kill"], stdout=subprocess.PIPE, check=True)
+except:
+    print("Error: failed to kill conflicting processes.")
+    sys.exit(1)
+print("Done.")
+
+# print_stdout(iwconfig)
 print("You did it!")
 
