aee055508fb86cd91d4d1dd1671fc5813241ea8e
Brian Cui <brian.cui@live.com>
2018-04-21 13:26:10 -0500

    Added port option to node server

--- web/server.js
+++ web/server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const args = process.argv.slice(2);
 
 if (!args[0]) {
-    console.log("Usage: node server.js <directory>");
+    console.log("Usage: node server.js <directory> [port=80]");
     process.exit(1);
 }
 
 const app = express();
+const port = args[1] ? parseInt(args[1], 10) : 80;
 app.get('/collector', (req, res) => {
-    console.log(`Request received: ${decodeURIComponent(req.originalUrl)}`);
+    console.log(`Collected: ${decodeURIComponent(req.originalUrl)}`);
     res.send("");
 });
 
 app.use(express.static(args[0]));
-app.listen(80, () => console.log("Collector listening on port 80!"));
-
+app.listen(port, () => {
+    console.log(`Node static webserver hosting ${args[0]} at localhost:${port}`);
+    console.log(`Collecting GET requests at localhost:${port}/collector`);
+});
